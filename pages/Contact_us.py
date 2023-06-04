fba69079fdabbaf442679efc013ca33fd4d82945
ravi-konjeti <71940212+ravi-konjeti@users.noreply.github.com>
2023-06-04 11:44:38 -0500

    Day 23: send email

--- pages/Contact_us.py
+++ pages/Contact_us.py
@@ -1,3 +1,12 @@
 import streamlit as st
 
 st.header("Contact Me")
+
+with st.form(key="email_forms"):
+    user_email = st.text_input("Your email address")
+    message = st.text_area("your message")
+    button = st.form_submit_button("Submit")
+    print(button)#button is false when it is not presssed and returns true once submit is pressed
+    if button:
+        message = message + user_email
+        print(button)
